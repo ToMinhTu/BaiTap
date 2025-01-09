49257ee2c1c9db1d08e0eafcec37392c06a0a052
ToMinhTu <68434628+ToMinhTu@users.noreply.github.com>
2025-01-09 09:51:15 +0700

    add dist

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import './assets/main.css'
-
-import { createApp } from 'vue'
-import App from './App.vue'
-import ButtonCustom from './components/ButtonCustom.vue'
-
-const app = createApp(App)
-app.component('button-custom', ButtonCustom)
-app.mount('#app')
